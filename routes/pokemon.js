5a8a60619a41f055e19d5fa60025b49d3a0f6371
danzmoses <danzmoses@gmail.com>
2022-10-21 20:23:30 -0700

    Added route to get a specific pokemon by name

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,14 +3,29 @@ const router = express.Router()
 const axios = require('axios')
 const BASE_URL = "https://pokeapi.co/api/v2"
 
+// All pokemon / Search pokemon
 router.get('/', async (req, res, next) => {
     try {
-        const pokemon = await axios.get(BASE_URL + '/pokemon/pikachu')
+        const pokemon = await axios.get(BASE_URL + `/pokemon/pikachu`)
+        res.render('pokemon/index', {
+            pokemon: pokemon.data
+        })
+    } catch (err) {
+        res.send('Error: ' + req.params.name + ' does not exist')
+        // next(err)
+    }
+})
+
+// Show one pokemon
+router.get('/:name', async (req, res, next) => {
+    try {
+        const pokemon = await axios.get(BASE_URL + `/pokemon/${req.params.name}`)
         res.render('pokemon/index', {
             pokemon: pokemon.data
         })
     } catch (err) {
         next(err)
+        res.send('Error: Pokemon ' + req.params.name + ' does not exist')
     }
 })
 
