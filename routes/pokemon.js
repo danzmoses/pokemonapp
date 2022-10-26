963993acf061ff3fbca960bb796e0a5bec8bd7df
danzmoses <danzmoses@gmail.com>
2022-10-26 12:49:38 -0700

    Index pokemon route can now get and display a list of pokemon Make sure to resolve all promises properly when using GET on external API

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,14 +5,33 @@ const BASE_URL = "https://pokeapi.co/api/v2"
 
 // All pokemon / Search pokemon
 router.get('/', async (req, res, next) => {
+
     try {
-        const pokemon = await axios.get(BASE_URL + `/pokemon/pikachu`)
+        // make API call to get list of pokemon
+        const pokemon = await axios.get(BASE_URL + `/pokemon?limit=50`)
+
+        // from the result, push all URLs to an array
+        endpoints = []
+        for (let i = 0; i < pokemon.data.results.length; ++i) {
+            endpoints.push(pokemon.data.results[i].url)
+        }
+
+        // display loading while waiting for promises to resolve
+        console.log('Loading...')
+
+        // get each individual pokemon from the list
+        const temp = await Promise.all(endpoints.map(url => axios(url).then(({data}) => data)))
+        
+        // render main page with retrieved pokemon
         res.render('pokemon/index', {
-            pokemon: pokemon.data
+            pokemon: temp
         })
+        next()
+
+        // remove loading after all promises resolve
+        console.log('Finished!')
     } catch (err) {
-        res.send('Error: ' + req.params.name + ' does not exist')
-        // next(err)
+        next(err)
     }
 })
 
