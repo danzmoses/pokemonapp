261fc8a406997a641eecdef578dd8d5c4fca8957
danzmoses <danzmoses@gmail.com>
2022-10-26 21:56:47 -0700

    Simple search added

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -7,24 +7,40 @@ const BASE_URL = "https://pokeapi.co/api/v2"
 router.get('/', async (req, res, next) => {
 
     try {
-        // make API call to get list of pokemon
-        const pokemon = await axios.get(BASE_URL + `/pokemon?limit=50`)
-
-        // from the result, push all URLs to an array
-        endpoints = []
-        for (let i = 0; i < pokemon.data.results.length; ++i) {
-            endpoints.push(pokemon.data.results[i].url)
+        // search query
+        let searchOptions = new RegExp()
+        if (req.query.name != null && req.query.name !== '') {
+            searchOptions = new RegExp(req.query.name, 'i')
         }
+
+        // make API call to get list of all pokemon
+        const pokemon = await axios.get(BASE_URL + `/pokemon?limit=1200&offset=0`)
+
+        console.log(pokemon.data.results)
+
+        // push URLs of pokemon that match regex to an array
+        endpoints = []
+        pokemon.data.results.forEach((res) => {
+            if (searchOptions.test(res.name))
+                endpoints.push(res.url)
+        })
+
+        // map each url to a promise
+        promises = endpoints.map(url => axios(url).then(
+            (({data}) => data)
+        ))
 
         // display loading while waiting for promises to resolve
         console.log('Loading...')
 
         // get each individual pokemon from the list
-        const temp = await Promise.all(endpoints.map(url => axios(url).then(({data}) => data)))
+        const pokeList = await Promise.all(promises)
         
+        console.log(promises.length)
+
         // render main page with retrieved pokemon
         res.render('pokemon/index', {
-            pokemon: temp
+            pokemon: pokeList
         })
         next()
 
