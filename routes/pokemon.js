909e66b87a5e81b35b65ba79f01f414aee1aff31
danzmoses <danzmoses@gmail.com>
2022-11-18 19:32:25 -0800

    Added more details to each pokemon's page Height converted to feet inches Weight converted to pounds Added Pokemon's type

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -59,6 +59,23 @@ function capitalizeFirstChar(string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
+function feetToInches(num) {
+    return num * 12
+}
+
+// Convert decimeters to feet inches
+function decimetersToFeetInches(height) {
+    height = height / 3.048
+    const feet = Math.floor(height)
+    const inches = Math.floor(feetToInches(height % 1))
+    return `${feet}' ${inches}"`
+}
+
+// Convert hectograms to pounds
+function hectogramsToPounds(weight) {
+    return weight / 4.536
+}
+
 // All pokemon / Search pokemon
 router.get('/', async (req, res, next) => {
 
@@ -103,9 +120,9 @@ router.get('/', async (req, res, next) => {
         
         console.log(promises.length)
 
-        pokeList.forEach(p => {
-            p.name = capitalizeFirstChar(p.name)
-        })
+        // pokeList.forEach(p => {
+        //     p.name = capitalizeFirstChar(p.name)
+        // })
 
         // render main page with retrieved pokemon
         res.render('pokemon/index', {
@@ -132,22 +149,26 @@ router.get('/:name', async (req, res, next) => {
             chain.push(capitalizeFirstChar(temp.species.name))
             temp = temp.evolves_to[0]
         }
-        const desc = species.data.flavor_text_entries[0].flavor_text
+        pokemon.data.chain = chain
+        pokemon.data.description = species.data.flavor_text_entries[0].flavor_text
         pokemon.data.name = capitalizeFirstChar(pokemon.data.name)
 
-        let genus
+        pokemon.data.types[0].type.name = capitalizeFirstChar(pokemon.data.types[0].type.name)
+        if (pokemon.data.types.length > 1) {
+            pokemon.data.types[1].type.name = capitalizeFirstChar(pokemon.data.types[1].type.name)
+        }
+
         species.data.genera.forEach(genera => {
             if (genera.language.name === 'en') {
-                genus = genera.genus
+                pokemon.data.genus = genera.genus
             }
         })
 
+        pokemon.data.height = decimetersToFeetInches(pokemon.data.height)
+        pokemon.data.weight = hectogramsToPounds(pokemon.data.weight).toFixed(1)
         
         res.render('pokemon/show', {
-            pokemon: pokemon.data,
-            chain: chain,
-            desc: desc,
-            genus: genus
+            pokemon: pokemon.data
         })
     } catch (err) {
         next(err)
