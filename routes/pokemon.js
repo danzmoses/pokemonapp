a162c4c4bd6daae2a015a846d9fc04df38444e63
danzmoses <danzmoses@gmail.com>
2023-04-06 11:46:45 -0700

    Added color to types

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -54,6 +54,27 @@ const generationMap = [
     }
 ]
 
+const typeToColor = {
+	normal: '#A8A77A',
+	fire: '#EE8130',
+	water: '#6390F0',
+	electric: '#F7D02C',
+	grass: '#7AC74C',
+	ice: '#96D9D6',
+	fighting: '#C22E28',
+	poison: '#A33EA1',
+	ground: '#E2BF65',
+	flying: '#A98FF3',
+	psychic: '#F95587',
+	bug: '#A6B91A',
+	rock: '#B6A136',
+	ghost: '#735797',
+	dragon: '#6F35FC',
+	dark: '#705746',
+	steel: '#B7B7CE',
+	fairy: '#D685AD',
+};
+
 // Make first character uppercase
 function capitalizeFirstChar(string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
@@ -123,9 +144,11 @@ router.get('/', async (req, res, next) => {
         
         pokeList.forEach(p => {
             // p['name'] = capitalizeFirstChar(p['name'])
-            p['type1'] = capitalizeFirstChar(p['types'][0]['type']['name'][0]);
+            p['type1'] = capitalizeFirstChar(p['types'][0]['type']['name']);
+            p['color1'] = capitalizeFirstChar(typeToColor[p['types'][0]['type']['name']]);
             if (Object.keys(p['types']).length > 1) {
-                p['type2'] = capitalizeFirstChar(p['types'][1]['type']['name'][0])
+                p['type2'] = capitalizeFirstChar(p['types'][1]['type']['name']);
+                p['color2'] = capitalizeFirstChar(typeToColor[p['types'][1]['type']['name']]);
             }         
         })
 
