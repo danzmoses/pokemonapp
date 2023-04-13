f711a1c32c5120773bd2db984fa44d76a6519245
danzmoses <danzmoses@gmail.com>
2023-04-12 23:24:40 -0700

    Added type filter

--- public/javascript/filterPokemon.js
+++ public/javascript/filterPokemon.js
@@ -1,18 +1,39 @@
 let pokemonContainers = document.querySelectorAll('.pokemon-container');
-let searchBar = document.querySelector('#search')
+let searchBar = document.querySelector('#search');
+let typeContainer = document.querySelector('#type');
 
-searchBar.addEventListener('keyup', function() {
+searchBar.addEventListener('keyup', filterPokemon);
+typeContainer.addEventListener('input', filterPokemon);
+
+function filterPokemon() {
+    // reset pokemon before applying filters
+    pokemonContainers.forEach(c => {
+        c.classList.remove('hide')
+    })
+
     let searchOptions = new RegExp();
     if (searchBar.value != null && searchBar.value !== '') {
         searchOptions = new RegExp(searchBar.value, 'i')
     }
+    let selectedType = typeContainer.value;
     pokemonContainers.forEach(c => {
         let pokemonName = c.querySelector('.pokemon-name-text').innerText;
-        if (searchOptions.test(pokemonName)) {
+        let pokemonTypes = c.querySelectorAll('table td')
+        let hasSelectedType = false
+        if (selectedType === 'All') {
+            hasSelectedType = true
+        }
+        else {
+            pokemonTypes.forEach(t => {
+                if (selectedType === t.innerText)
+                    hasSelectedType = true;
+            })
+        }
+        if (searchOptions.test(pokemonName) && hasSelectedType) {
                 c.classList.remove('hide');
         }
         else {
             c.classList.add('hide');
         }
     })
-});
+}
